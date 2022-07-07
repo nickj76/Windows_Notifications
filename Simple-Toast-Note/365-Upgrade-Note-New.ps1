453a13b5c636aac1cf9791697176bf2afcfb4170
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-07-07 11:20:26 +0100

    updated

--- Simple-Toast-Note/365-Upgrade-Note-New.ps1
+++ Simple-Toast-Note/365-Upgrade-Note-New.ps1
@@ -301,4 +301,4 @@ If(!(test-path $logfilespath))
       New-Item -ItemType Directory -Force -Path $logfilespath
 }
 
-New-Item -ItemType "file" -Path "c:\logfiles\Office-Upgrade-050722.txt"
+New-Item -ItemType "file" -Path "c:\logfiles\Office-Upgrade-070722.txt"
